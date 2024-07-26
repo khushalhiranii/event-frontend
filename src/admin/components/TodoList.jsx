cae902a43254493b0db402e57dedc0dc175f97a6
khushalhiranii <khushal.hirani4@gmail.com>
2024-07-26 23:44:49 +0530

    form sorted

--- src/admin/components/TodoList.jsx
+++ src/admin/components/TodoList.jsx
@@ -5,7 +5,7 @@ import './TodoList.css';
 import { useEvents } from '../../context/EventContext';
 
 const TodoList = () => {
-  console.log("Hi Hi Hi")
+  console.log('Hi Hi Hi');
   const { events, deleteEvent } = useEvents();
   console.log('events:', events); // Debugging log
   console.log('deleteEvent:', deleteEvent); // Debugging log
@@ -13,14 +13,14 @@ const TodoList = () => {
   const navigate = useNavigate();
 
   const copyToClipboard = async (url) => {
-  
     try {
       await navigator.clipboard.writeText(url);
       alert('URL copied to clipboard!');
     } catch (err) {
       console.error('Failed to copy: ', err);
     }
-  }
+  };
+
   const addTodo = () => {
     navigate('/dashboard/add');
   };
@@ -36,7 +36,7 @@ const TodoList = () => {
             <p>{event.eventDate}</p>
           </div>
           <div>
-            <button onClick={copyToClipboard(`https://event-frontend-omega.vercel.app/events/${event.id}`)}>
+            <button onClick={() => copyToClipboard(`https://event-frontend-omega.vercel.app/events/${event.id}`)}>
               Copy Form URL
             </button>
             <Link to={`/dashboard/edit/${event.id}`}>
