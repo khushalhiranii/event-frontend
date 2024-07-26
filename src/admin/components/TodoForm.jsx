a58e53d9c4b62a9f42763c75db1797c6796f6c5b
khushalhiranii <khushal.hirani4@gmail.com>
2024-07-26 17:09:35 +0530

    todoform

--- src/admin/components/TodoForm.jsx
+++ src/admin/components/TodoForm.jsx
@@ -21,7 +21,7 @@ const TodoForm = () => {
   });
   const [isDataLoaded, setIsDataLoaded] = useState(false); // State to track data loading
 
-  useEffect(async() => {
+  useEffect(() => {
     if (id) {
       const existingTodo = events.find((t) => t.id === parseInt(id, 10));
       if (existingTodo) {
