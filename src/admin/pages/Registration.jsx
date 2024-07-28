d8413dcc4d035f549bb0bf6d79e8c41bad27ff7a
khushalhiranii <khushal.hirani4@gmail.com>
2024-07-29 04:28:49 +0530

    registered

--- src/admin/pages/Registration.jsx
+++ src/admin/pages/Registration.jsx
@@ -15,7 +15,7 @@ const Registration = () => {
         setLoading(true);
         const response = await apiClient.get(`/admin/event/users/${id}`); // Replace with your API endpoint
         console.log(response)
-        setRegisteredUsers(response.data);
+        setRegisteredUsers(response.data.data);
       } catch (err) {
         setError('Failed to fetch registered users');
       } finally {
@@ -38,8 +38,8 @@ const Registration = () => {
         <ul>
           {registeredUsers.map((user) => (
             <li key={user.id} className="border-b py-2">
-              <p><strong>Name:</strong> {user.name}</p>
-              <p><strong>Phone:</strong> {user.phone}</p>
+              <p><strong>Name:</strong> {user.userName}</p>
+              <p><strong>Phone:</strong> {user.phoneNo}</p>
             </li>
           ))}
         </ul>
