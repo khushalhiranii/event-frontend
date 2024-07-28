29c42451167e19101e1f3082901d7a340d939293
khushalhiranii <khushal.hirani4@gmail.com>
2024-07-29 00:34:14 +0530

    axios

--- src/admin/axiosSetup.jsx
+++ src/admin/axiosSetup.jsx
@@ -4,6 +4,9 @@ import axios from 'axios';
 // Create an Axios instance
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL, // Replace with your API base URL
+  headers: {
+    "Content-Type": "application/json",
+  },
   withCredentials: true, // Enable cookie management
 });
 
