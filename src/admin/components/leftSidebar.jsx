5fdae8b6bcaa4b2dff382363c5f48c584e569c90
khushalhiranii <khushal.hirani4@gmail.com>
2024-07-29 22:53:32 +0530

    sidebar ht

--- src/admin/components/leftSidebar.jsx
+++ src/admin/components/leftSidebar.jsx
@@ -85,7 +85,7 @@ const Sidebar = ({ className = "" }) => {
         </NavLink>
       </div>
       
-        <div className="self-stretch h-[27.5rem] flex flex-col items-start justify-start pt-[0rem] px-[0rem] box-border gap-[0.25rem] z-[2]">
+        <div className="self-stretch flex flex-col items-start justify-start pt-[0rem] px-[0rem] box-border gap-[0.25rem] z-[2]">
         {events.map((event) => (
           <div className="w-full">
           <button
