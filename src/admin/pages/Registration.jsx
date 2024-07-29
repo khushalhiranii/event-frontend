1464db71c46705ff614a3d3339b1ff7e94b18337
khushalhiranii <khushal.hirani4@gmail.com>
2024-07-29 15:51:25 +0530

    table updated

--- src/admin/pages/Registration.jsx
+++ src/admin/pages/Registration.jsx
@@ -34,21 +34,52 @@ const Registration = () => {
       {registeredUsers.length === 0 ? (
         <p>No registered users found.</p>
       ) : (
-        <table className="min-w-full bg-white border">
-          <thead>
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th className="py-2 px-4 border-b">Name</th>
-              <th className="py-2 px-4 border-b">Phone No.</th>
-              <th className="py-2 px-4 border-b">Mode</th>
+              <th scope="col" className="p-4">
+                <div className="flex items-center">
+                  <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                  <label htmlFor="checkbox-all-search" className="sr-only">checkbox</label>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Phone No.
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Mode
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
             </tr>
           </thead>
           <tbody>
             {registeredUsers.map((user) => (
-              <tr key={user.id}>
-                <td className="py-2 px-4 border-b">{user.userName}</td>
-                <td className="py-2 px-4 border-b">{user.phoneNo}</td>
-                <td className="py-2 px-4 border-b">{user.modeOfRegistration}</td>
-              </tr>
+              <tr key={user.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+              <td className="w-4 p-4">
+                <div className="flex items-center">
+                  <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                  <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
+                </div>
+              </td>
+              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {user.userName}
+              </th>
+
+              <td className="px-6 py-4">
+                {user.phoneNo}
+              </td>
+              <td className="px-6 py-4">
+                {user.modeOfRegistration}
+              </td>
+              <td className="px-6 py-4">
+                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+              </td>
+            </tr>
             ))}
           </tbody>
         </table>
