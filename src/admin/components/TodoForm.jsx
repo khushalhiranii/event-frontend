3b28204190a9b4eae92d0e8c25acd6efeb32bad0
khushalhiranii <khushal.hirani4@gmail.com>
2024-07-31 04:21:23 +0530

    todoform

--- src/admin/components/TodoForm.jsx
+++ src/admin/components/TodoForm.jsx
@@ -7,11 +7,12 @@ import { DateRangePicker } from 'react-date-range';
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
 import { useLoading } from '../../context/Loadingcontext';
+import '../styles/TodoForm.css';
 
 const TodoForm = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const { events, addEvent, updateEvent } = useEvents();
+  const { addEvent, updateEvent } = useEvents();
   const { startLoading, stopLoading } = useLoading();
   const [currentStep, setCurrentStep] = useState(1); // State to track current step
   const [todo, setTodo] = useState({
@@ -21,7 +22,7 @@ const TodoForm = () => {
     endDate: '',
     userJourney: ['Attendance', 'Food', 'Kit'],
     eventTemplate: `[{"id":"E719EE2B-9385-4AF2-81D8-586A6A835FDB","element":"TextInput","text":"Text Input","required":true,"canHaveAnswer":true,"canHavePageBreakBefore":true,"canHaveAlternateForm":true,"canHaveDisplayHorizontal":true,"canHaveOptionCorrect":true,"canHaveOptionValue":true,"canPopulateFromApi":true,"field_name":"text_input_103DC733-9828-4C8D-BDD5-E2BCDD96D92A","label":"Name ","dirty":false},{"id":"DBABFE94-29D7-47E2-9ECD-54AE7AFF3391","element":"PhoneNumber","text":"Phone Number","required":true,"canHaveAnswer":true,"canHavePageBreakBefore":true,"canHaveAlternateForm":true,"canHaveDisplayHorizontal":true,"canHaveOptionCorrect":true,"canHaveOptionValue":true,"canPopulateFromApi":true,"field_name":"phone_input_0A6EEDDB-E0D5-4BC7-8D4B-CF2D4896B786","label":"Phone Number","dirty":false},{"id":"48C6DE8C-B312-4A40-A4A4-725B2242C109","element":"EmailInput","text":"Email","required":true,"canHaveAnswer":true,"canHavePageBreakBefore":true,"canHaveAlternateForm":true,"canHaveDisplayHorizontal":true,"canHaveOptionCorrect":true,"canHaveOptionValue":true,"canPopulateFromApi":true,"field_name":"email_input_A4A11559-34CB-4A95-BB86-E89C8CABE06C","label":"E-Mail","dirty":false}]`,
-    attendieType: ['Audience', 'NRI'],
+    attendieType: [],
     address: ''
   });
   const [isDataLoaded, setIsDataLoaded] = useState(false);
@@ -53,13 +54,23 @@ const TodoForm = () => {
         setIsDataLoaded(true);
       }
     }
-  }, [id, events]);
+  }, [id]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setTodo({
       ...todo,
       [name]: value,
+    });
+  };
+
+  const handleCheckboxChange = (e) => {
+    const { value, checked } = e.target;
+    setTodo((prevTodo) => {
+      const updatedAttendieType = checked
+        ? [...prevTodo.attendieType, value]
+        : prevTodo.attendieType.filter((type) => type !== value);
+      return { ...prevTodo, attendieType: updatedAttendieType };
     });
   };
 
@@ -79,7 +90,7 @@ const TodoForm = () => {
       return !isNaN(date);
     };
 
-    const isFormFilled = todo.eventName && todo.startDate && todo.endDate && todo.address;
+    const isFormFilled = todo.eventName && todo.startDate && todo.endDate && todo.address && todo.attendieType.length > 0;
     const isDateRangeValid = new Date(todo.startDate) <= new Date(todo.endDate);
 
     setIsFormValid(isFormFilled && isValidDate(todo.startDate) && isDateRangeValid);
@@ -102,16 +113,14 @@ const TodoForm = () => {
   
     try {
       // Determine if we are updating or adding a new event
-      startLoading()
+      startLoading();
       if (id) {
         await updateEvent(parseInt(id, 10), formData);
-        stopLoading();
       } else {
-        console.log(formData)
+        console.log(formData);
         await addEvent(formData);
-        stopLoading();
       }
-  
+      stopLoading();
       // Navigate to the dashboard after successful submission
       navigate('/dashboard');
     } catch (error) {
@@ -119,7 +128,6 @@ const TodoForm = () => {
       // You can also add error handling here, e.g., displaying a notification or message
     }
   };
-  
 
   const handleSave = (data) => {
     setTodo((prevTodo) => ({
@@ -149,70 +157,99 @@ const TodoForm = () => {
   };
 
   return (
-    <div className='p-10 font-semibold'>
+    <div className="todo-form">
       {currentStep === 1 && (
-        <form className='w-full space-y-4' onSubmit={(e) => e.preventDefault()}>
-          <div className='flex justify-end mt-4'>
-            <button type='button' disabled={!isFormValid} onClick={handleNextStep} className="mt-4 px-4 py-2 rounded">
-              Next
-            </button>
-          </div>
-          <div>
-            <label>Event Name</label>
+        <form className="todo-form__container" onSubmit={(e) => e.preventDefault()}>
+          <h2 className="todo-form__header">Event Details</h2>
+          <div className="todo-form__group">
+            <label htmlFor="eventName" className="todo-form__label">Event Name</label>
             <input
-              className='border border-black'
-              type='text'
-              name='eventName'
+              type="text"
+              name="eventName"
               value={todo.eventName}
               onChange={handleChange}
+              className="todo-form__input"
               required
             />
           </div>
-          <div>
-            <label>Event Address</label>
+          <div className="todo-form__group">
+            <label htmlFor="address" className="todo-form__label">Event Address</label>
             <input
-              className='border border-black'
-              type='text'
-              name='address'
+              type="text"
+              name="address"
               value={todo.address}
               onChange={handleChange}
+              className="todo-form__input"
               required
             />
           </div>
-          <div className='w-full'>
-            <label>Event Date Range</label>
-            <div>
-              <DateRangePicker
-                ranges={[{
-                  startDate: isNaN(new Date(todo.startDate)) ? getCurrentISTDate() : new Date(todo.startDate),
-                  endDate: isNaN(new Date(todo.endDate)) ? getCurrentISTDate() : new Date(todo.endDate),
-                  key: 'selection',
-                }]}
-                direction="horizontal"
-                showSelectionPreview={true}
-                moveRangeOnFirstSelection={false}
-                months={1}
-                onChange={handleDateChange}
-                className='w-full'
-                minDate={getCurrentISTDate()}
-              />
+          <div className="todo-form__group">
+            <label className="todo-form__label">Event Date Range</label>
+            <DateRangePicker
+              ranges={[{
+                startDate: isNaN(new Date(todo.startDate)) ? getCurrentISTDate() : new Date(todo.startDate),
+                endDate: isNaN(new Date(todo.endDate)) ? getCurrentISTDate() : new Date(todo.endDate),
+                key: 'selection',
+              }]}
+              direction="horizontal"
+              showSelectionPreview={true}
+              moveRangeOnFirstSelection={false}
+              months={1}
+              onChange={handleDateChange}
+              className="todo-form__date-range"
+              minDate={getCurrentISTDate()}
+            />
+          </div>
+          <div className="todo-form__group">
+            <label className="todo-form__label">Attendee Types</label>
+            <div className="todo-form__checkbox-group">
+              {['Audience', 'Speaker', 'Delegate', 'NRI'].map((type) => (
+                <div key={type} className="todo-form__checkbox-item">
+                  <label>
+                    <input
+                      type="checkbox"
+                      value={type}
+                      checked={todo.attendieType.includes(type)}
+                      onChange={handleCheckboxChange}
+                    />
+                    {type}
+                  </label>
+                </div>
+              ))}
             </div>
+          </div>
+          <div className="todo-form__footer">
+            <button
+              type="button"
+              disabled={!isFormValid}
+              onClick={handleNextStep}
+              className="todo-form__button"
+            >
+              Next
+            </button>
           </div>
         </form>
       )}
       {currentStep === 2 && (
-        <div>
-          <div className='flex justify-between mt-1'>
-            <button type='button' onClick={handlePreviousStep} className="bg-gray-500 text-white px-4 py-2 rounded">
+        <div className="todo-form__container">
+          <h2 className="todo-form__header">Customize Form</h2>
+          <ReactFormBuilder saveUrl="" onPost={handleSave} onLoad={handleLoad} />
+          <div className="todo-form__footer">
+            <button
+              type="button"
+              onClick={handlePreviousStep}
+              className="todo-form__button"
+            >
               Previous
             </button>
-            <button type="submit" onClick={handleSubmit} className="px-4 py-2 rounded">
+            <button
+              type="submit"
+              onClick={handleSubmit}
+              className="todo-form__button"
+            >
               {id ? "Update" : "Create"} Event
             </button>
           </div>
-          <div className='mt-3'>
-            <ReactFormBuilder saveUrl='' onPost={handleSave} onLoad={handleLoad} />
-            </div>
         </div>
       )}
     </div>
