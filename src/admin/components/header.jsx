d44d22b65259c572d6340ec288fe7fdfd14fc1b4
khushalhiranii <khushal.hirani4@gmail.com>
2024-08-02 00:39:51 +0530

    no-print

--- src/admin/components/header.jsx
+++ src/admin/components/header.jsx
@@ -21,7 +21,7 @@ const Header = ({
 
   return (
     <div
-      className={`fixed h-[68px] z-40 bg-white flex flex-row items-center justify-between py-boundvariablesdata10 px-boundvariablesdata5 text-left text-[0.75rem] text-white font-semibold border-b-[1px] border-solid border-black-10 ${className}`}
+      className={`no-print fixed h-[68px] z-40 bg-white flex flex-row items-center justify-between py-boundvariablesdata10 px-boundvariablesdata5 text-left text-[0.75rem] text-white font-semibold border-b-[1px] border-solid border-black-10 ${className}`}
       style={{ width: 'calc(100vw - 16rem)' }}
     >
       <div className="flex flex-row items-center justify-start gap-[0.5rem]">
