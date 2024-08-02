7bb1a875b5fae7a6a922eb465cc33aba4773034a
khushalhiranii <khushal.hirani4@gmail.com>
2024-08-02 12:43:58 +0530

    agent list

--- src/admin/pages/AgentList.jsx
+++ src/admin/pages/AgentList.jsx
@@ -1,5 +1,5 @@
 // src/components/AgentList.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAgents } from '../context/AgentContext';
 import '../styles/AgentList.css';
