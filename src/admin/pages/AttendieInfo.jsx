4bd75542c1d7d7e65bbce5fa42e7551c8e63cedc
khushalhiranii <khushal.hirani4@gmail.com>
2024-08-02 12:05:19 +0530

    attendie info

--- src/admin/pages/AttendieInfo.jsx
+++ src/admin/pages/AttendieInfo.jsx
@@ -1,7 +1,8 @@
 // src/components/AttendieInfo.jsx
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useRegisteredUsers } from '../context/RegisteredUsersContext';
+import ReactToPrint from 'react-to-print';
 import '../styles/AttendieInfo.css'; // Ensure you create this CSS file
 
 const AttendieInfo = () => {
@@ -9,6 +10,7 @@ const AttendieInfo = () => {
   const { registeredUsers } = useRegisteredUsers();
   const [attendie, setAttendie] = useState(null);
   const [formValues, setFormValues] = useState([]);
+  const componentRef = useRef();
 
   useEffect(() => {
     const user = registeredUsers.find(user => user.id === parseInt(attendieId, 10));
@@ -23,24 +25,24 @@ const AttendieInfo = () => {
     }
   }, [attendieId, registeredUsers]);
 
-  const handlePrint = () => {
-    window.print();
-  };
-
   if (!attendie) return <p>Loading...</p>;
 
   return (
     <div className="attendie-info-container p-4">
       <h1 className="text-xl font-bold mb-4">Attendie Info</h1>
       <div className="no-print">
-        <button onClick={handlePrint} className="bg-blue-500 text-white py-2 px-4 rounded">Print</button>
+        {/* Non-printable content, like buttons */}
+        <ReactToPrint
+          trigger={() => (
+            <button className="bg-blue-500 text-white py-2 px-4 rounded">
+              Print
+            </button>
+          )}
+          content={() => componentRef.current}
+        />
       </div>
-      <div className="print-container">
-        <img src={attendie.QR}></img>
-        {/* <h1 className="print-title">Attendie Information</h1>
-        <p className="print-detail"><strong>Name:</strong> {attendie.userName}</p>
-        <p className="print-detail"><strong>Phone No.:</strong> {attendie.phoneNo}</p>
-        <p className="print-detail"><strong>Mode of Registration:</strong> {attendie.modeOfRegistration}</p> */}
+      <div className="print-container" ref={componentRef}>
+        <img src={attendie.QR} alt="QR Code" />
         <div className="print-detail mt-4">
           <h2 className="text-lg font-semibold mb-2">Form Values:</h2>
           <div className="form-values-list">
