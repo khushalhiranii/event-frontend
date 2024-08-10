7129733aae64553f9cda8eeb7cd1a153ac7315c0
khushalhiranii <khushal.hirani4@gmail.com>
2024-08-10 18:29:51 +0530

    auth updated

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -121,20 +121,20 @@ export const AuthProvider = ({ children }) => {
       });
       console.log(response)
       const data = await response.json();
-      // console.log(data.data)
+      console.log(data.data)
       // const accessToken = data.data.accessToken;
 
-      // const accessToken2 = response.headers.get('accessToken');
-      // const refreshToken = response.headers.get('refreshToken');
-      // console.log(`header ${accessToken2}`)
-      // if (accessToken2) {
-      //   console.log('Access Token:', accessToken2);
-      //   // Store the token, e.g., in local storage or state
-      //   localStorage.setItem('accessToken', accessToken2);
-      //   localStorage.setItem('refreshToken', refreshToken);
-      // } else {
-      //   console.error('No access token found in response headers');
-      // }
+      const accessToken2 = response.headers.get('accessToken');
+      const refreshToken = response.headers.get('refreshToken');
+      console.log(`header ${accessToken2}`)
+      if (accessToken2) {
+        console.log('Access Token:', accessToken2);
+        // Store the token, e.g., in local storage or state
+        localStorage.setItem('accessToken', accessToken2);
+        localStorage.setItem('refreshToken', refreshToken);
+      } else {
+        console.error('No access token found in response headers');
+      }
 
       if (response.ok) {
         // setAccessToken(data.accessToken);
